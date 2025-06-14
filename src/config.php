6a2b62cdcd0cf455859db8e368aa2f25e7787ea3
YujunLuo <k4917552@gmail.com>
2025-06-14 11:23:22 +0800

    Delete src directory

--- src/config.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('DB_HOST', getenv('DB_HOST') ?: 'localhost');
-define('DB_NAME', getenv('DB_NAME') ?: 'ecommerce');
-define('DB_USER', getenv('DB_USER') ?: 'root');
-define('DB_PASS', getenv('DB_PASS') ?: 'password');
-?>
